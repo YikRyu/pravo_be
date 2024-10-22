ab914e670d6f453204718a36a6c9767e94c2f691
Tan Zhi Shan <heliosjun@ymail.com>
2024-10-22 23:36:32 +0800

    Remove recognition from RewardTransation entry Dto

--- src/main/java/com/example/pravo/dto/RewardTransactionEntryDto.java
+++ src/main/java/com/example/pravo/dto/RewardTransactionEntryDto.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RewardTransactionEntryDto {
     private Integer points;
-    private Long recognitionId;
     private String rewards;
     private String userId;
 }
